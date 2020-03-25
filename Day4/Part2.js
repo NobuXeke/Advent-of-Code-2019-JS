d846fcafae20047b781a4c7e00badf3457ce2eed
NobuXeke <60720710+NobuXeke@users.noreply.github.com>
2020-03-25 08:09:48 -0300

    Part2

---
+++ Day4/Part2.js
@@ -0,0 +1,25 @@
+function findPossiblePasswords() {
+	let passwords = [];
+	for (let i = 124075; i <= 580769; i++) {
+		let password = i.toString();
+		for (let a = 1; a < password.length; a++) {
+			if (password[a] < password[a - 1]) {
+				break;
+			}
+			if (a === 5) {
+				for (let b = 1; b < password.length; b++) {
+					if (password[b] === password[b - 1]) {
+						if (password[b] === password[b + 1] || password[b] === password[b - 2]) {
+							continue;
+						}
+						passwords.push(Number(password));
+						break;
+					}
+				}
+			}
+		}
+	}
+	return passwords;
+}
+console.log(findPossiblePasswords());
+console.log(findPossiblePasswords().length);
